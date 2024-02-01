d60722c85c08ce70592a021650a56c9911d59b07
Keenan <110526824+Keeks87@users.noreply.github.com>
2024-02-01 17:26:08 +0200

    Push server changes

--- index.js
+++ index.js
@@ -22,23 +22,27 @@ const routes = require("./routes");
 // Server setup
 const app = express();
 
-// Middleware configuration
-app.use(cors({ origin: 'http://localhost:3000' }));
+// Dynamic port for deployment
+const PORT = process.env.PORT || 5000;
+
+// Dynamic CORS origin
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+};
+app.use(cors(corsOptions));
 
 // Middleware
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", corsOptions.origin);
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
 // MongoDB connection
-mongoose.connect(
-  "mongodb+srv://keenandup87:1234@cluster0.50yztom.mongodb.net/",
-  {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
@@ -53,5 +57,5 @@ app.use(routes);
 
 // Server start
 app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+  console.log(`Server is running on port ${PORT}`);
 });
