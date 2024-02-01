e139cb63a66f8ee77d969162e2ff4ef27bc721ae
Keenan <110526824+Keeks87@users.noreply.github.com>
2024-02-01 23:13:25 +0200

    Update cors

--- index.js
+++ index.js
@@ -25,15 +25,18 @@ const app = express();
 // Dynamic port for deployment
 const PORT = process.env.PORT || 5000;
 
-// Dynamic CORS origin
+// Updated CORS Options
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  origin: 'https://eventsmanage-front.onrender.com',
+  optionsSuccessStatus: 200 // For legacy browser support
 };
+
 app.use(cors(corsOptions));
 
 // Middleware
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", corsOptions.origin);
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
@@ -56,6 +59,6 @@ db.once("open", () => {
 app.use(routes);
 
 // Server start
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
